6b760301585ac89ab8e56f59d04eb2f829e1c24d
Iara De Juanini <solajuega@gmail.com>
2022-11-05 19:54:16 -0300

    cambios en el model realizados

--- app/model/games.model.php
+++ app/model/games.model.php
@@ -5,12 +5,11 @@ class GamesModel {
         $db = new PDO('mysql:host=localhost;'.'dbname=db-games; charset=utf8', 'root', '');
         return $db;
     }
+
     function getGames(){
         $db = $this->connect();
-        
         $query = $db->prepare("SELECT * FROM `gamesstudent`");
         $query->execute();
-
         return $query->fetchAll(PDO::FETCH_OBJ);
     }
 
